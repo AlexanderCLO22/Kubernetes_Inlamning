d0a5b60ef6e91412d5e6e3d8c5c5a33f06caf8b3
Alexander Öjersson <98aleoje@gafe.molndal.se>
2023-10-06 15:44:01 +0200

    argo

---
+++ ArgoCD/configure_argocd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
+echo "Your argocd password is: $(argocd admin initial-password -n argocd)"
+
