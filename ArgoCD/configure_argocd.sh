eb759073e86a2058ce7da3dd453ed2d2d8ba1065
Alexander Öjersson <98aleoje@gafe.molndal.se>
2023-10-08 18:19:36 +0200

    updaterar manifest

--- ArgoCD/configure_argocd.sh
+++ ArgoCD/configure_argocd.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 kubectl create namespace argocd
+
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
-echo "Your argocd password is: $(argocd admin initial-password -n argocd)"
+echo "Your argocd password is:"
+
+argocd admin initial-password -n argocd
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443
 
